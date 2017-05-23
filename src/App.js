44aee51c179fa332eea9e383b4ba9023c3a3227f
Gleb <mail@g1eb.com>
2017-05-23 13:50:41 +0200

    Added container and sample components to the app.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import Sample from './components/Sample'
+import Container from './components/Container'
+
 import './styles/app.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="container">
-      </div>
+      <Container>
+        <Sample src="" name="sample 1" color="#FF875C" />
+        <Sample src="" name="sample 2" color="#FF7847" />
+        <Sample src="" name="sample 3" color="#FF6933" />
+        <Sample src="" name="sample 4" color="#FF5A1F" />
+        <Sample src="" name="sample 5" color="#FF4500" />
+        <Sample src="" name="sample 6" color="#70D7FF" />
+        <Sample src="" name="sample 7" color="#5CD1FF" />
+        <Sample src="" name="sample 8" color="#47CBFF" />
+        <Sample src="" name="sample 9" color="#33C5FF" />
+        <Sample src="" name="sample 10" color="#1FBFFF" />
+        <Sample src="" name="sample 11" color="#C247FF" />
+        <Sample src="" name="sample 12" color="#BB33FF" />
+        <Sample src="" name="sample 13" color="#B41FFF" />
+        <Sample src="" name="sample 14" color="#AD0AFF" />
+        <Sample src="" name="sample 15" color="#A300F5" />
+      </Container>
     );
   }
 }
