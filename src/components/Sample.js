048686735f6bd0e74f1064dc78094c4c298ddbdd
Gleb <mail@g1eb.com>
2017-05-23 10:42:54 +0200

    Added initial container and sample components.

---
+++ src/components/Sample.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class Sample extends React.Component {
+
+  handleClick() {
+    console.log(`clicked on sample: ${this.props.name}`);
+  }
+
+  render() {
+    return (
+      <div
+        className="sample"
+        style={{background: this.props.color}}
+        onClick={(event) => this.handleClick()}>
+      </div>
+    )
+  }
+}
+
+Sample.defaultProps = {
+  src: '',
+  name: '',
+  color: '',
+}
+
+export default Sample
