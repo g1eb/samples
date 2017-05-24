43ccd4917f5a9dc6b38de1ef0b644e10eaaa5426
Gleb <mail@g1eb.com>
2017-05-24 22:03:04 +0200

    Refactored container layout to pass array of samples on to the row component.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
-import React from 'react';
+import React from 'react'
 
 import Row from './components/Row'
-import Sample from './components/Sample'
 import Container from './components/Container'
 
-import './styles/app.css';
+import './styles/app.css'
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.samples = [
+      {'src': '/sounds/11.mp3', 'name': 'sample 11', 'color': '#FF875C'},
+      {'src': '/sounds/12.mp3', 'name': 'sample 12', 'color': '#FF7847'},
+      {'src': '/sounds/13.mp3', 'name': 'sample 13', 'color': '#FF6933'},
+      {'src': '/sounds/14.mp3', 'name': 'sample 14', 'color': '#FF5A1F'},
+      {'src': '/sounds/15.mp3', 'name': 'sample 15', 'color': '#FF4500'},
+      {'src': '/sounds/21.mp3', 'name': 'sample 21', 'color': '#70D7FF'},
+      {'src': '/sounds/22.mp3', 'name': 'sample 22', 'color': '#5CD1FF'},
+      {'src': '/sounds/23.mp3', 'name': 'sample 23', 'color': '#47CBFF'},
+      {'src': '/sounds/24.mp3', 'name': 'sample 24', 'color': '#33C5FF'},
+      {'src': '/sounds/25.mp3', 'name': 'sample 25', 'color': '#1FBFFF'},
+      {'src': '/sounds/31.mp3', 'name': 'sample 31', 'color': '#C247FF'},
+      {'src': '/sounds/32.mp3', 'name': 'sample 32', 'color': '#BB33FF'},
+      {'src': '/sounds/33.mp3', 'name': 'sample 33', 'color': '#B41FFF'},
+      {'src': '/sounds/34.mp3', 'name': 'sample 34', 'color': '#AD0AFF'},
+      {'src': '/sounds/35.mp3', 'name': 'sample 35', 'color': '#A300F5'},
+    ]
+  }
+
+  componentWillMount() {
+    this.context = new (window.AudioContext || window.webkitAudioContext)()
+  }
+
   render() {
     return (
       <Container>
-        <Row>
-          <Sample src="" name="sample 1" color="#FF875C" />
-          <Sample src="" name="sample 2" color="#FF7847" />
-          <Sample src="" name="sample 3" color="#FF6933" />
-          <Sample src="" name="sample 4" color="#FF5A1F" />
-          <Sample src="" name="sample 5" color="#FF4500" />
-        </Row>
-        <Row>
-          <Sample src="" name="sample 6" color="#70D7FF" />
-          <Sample src="" name="sample 7" color="#5CD1FF" />
-          <Sample src="" name="sample 8" color="#47CBFF" />
-          <Sample src="" name="sample 9" color="#33C5FF" />
-          <Sample src="" name="sample 10" color="#1FBFFF" />
-        </Row>
-        <Row>
-          <Sample src="" name="sample 11" color="#C247FF" />
-          <Sample src="" name="sample 12" color="#BB33FF" />
-          <Sample src="" name="sample 13" color="#B41FFF" />
-          <Sample src="" name="sample 14" color="#AD0AFF" />
-          <Sample src="" name="sample 15" color="#A300F5" />
-        </Row>
+        <Row context={this.context} samples={this.samples.slice(0, 5)} />
+        <Row context={this.context} samples={this.samples.slice(5, 10)} />
+        <Row context={this.context} samples={this.samples.slice(10, 15)} />
       </Container>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
+import Sample from './Sample'
+
 import '../styles/row.css'
 
 class Row extends React.Component {
   render() {
     return (
       <div className="row">
-        { this.props.children }
+        {
+          this.props.samples.map((sample) => {
+            return <Sample src={sample.src} name={sample.name} color={sample.color} context={this.props.context} />
+          })
+        }
       </div>
     )
   }
