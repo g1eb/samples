48786e6fa0f3aebf91947c1f381db2969e83c724
Gleb <mail@g1eb.com>
2017-05-24 11:03:17 +0200

    Added animation to the samples.

--- src/components/Sample.js
+++ src/components/Sample.js
@@ -10,21 +10,40 @@ class Sample extends React.Component {
 
     this.state = {
       hidden: true,
+      animate: false,
     }
+  }
 
+  componentDidMount() {
     window.setTimeout(() => {
       this.setState({hidden: false})
     }, Math.round(Math.random() * 1000))
+
+    this.animate()
+  }
+
+  animate() {
+    window.setTimeout(() => {
+      this.toggleAnimate()
+      this.animate()
+    }, Math.ceil(Math.random() * 25) * 1000)
+  }
+
+  toggleAnimate() {
+    this.setState({animate: true})
+    window.setTimeout(() => {
+      this.setState({animate: false})
+    }, 1000)
   }
 
   handleClick() {
-    console.log(`clicked on sample: ${this.props.name}`);
+    console.log(`clicked on sample: ${this.props.name}`)
   }
 
   render() {
     return (
       <div
-        className={ classNames('sample', {'hidden': this.state.hidden}) }
+        className={ classNames('sample', {'hidden': this.state.hidden, 'animate': this.state.animate}) }
         style={{background: this.props.color}}
         onClick={(event) => this.handleClick()}>
       </div>
