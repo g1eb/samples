ad41fb05c53cc68d4b7dea61bf230a8197d7d076
Gleb <mail@g1eb.com>
2017-05-25 11:21:02 +0200

    Use promises to wait for all samples to be loaded before playback.

--- src/services/audio.js
+++ src/services/audio.js
@@ -5,29 +5,37 @@ class AudioService {
 
     this.buffers = []
 
-    samples.map((sample) => this.fetchBuffer(sample.name, sample.src))
+    let promises = samples.map((sample) => this.fetchBuffer(sample.name, sample.src))
+    Promise.all(promises).then(() => {
+      this.ready = true
+    })
   }
 
   fetchBuffer(name, url) {
-    var request = new XMLHttpRequest()
-    request.open('GET', url, true)
-    request.responseType = 'arraybuffer'
-    request.onload = (data) => {
-      this.context.decodeAudioData(request.response, (buffer) => {
-        this.buffers[name] = buffer
-      }, (err) => {
-        console.log('sample service err: ', err)
-      })
-    }
-    request.send()
+    return new Promise((resolve, reject) => {
+      var request = new XMLHttpRequest()
+      request.open('GET', url, true)
+      request.responseType = 'arraybuffer'
+      request.onload = (data) => {
+        this.context.decodeAudioData(request.response, (buffer) => {
+          this.buffers[name] = buffer
+          resolve()
+        }, (err) => {
+          reject(`sample service err: ${err}`)
+        })
+      }
+      request.send()
+    })
   }
 
   play(name) {
-    var source = this.context.createBufferSource()
-    source.connect(this.context.destination)
-    source.buffer = this.buffers[name]
-    source.loop = false
-    source.start(0)
+    if ( !!this.ready ) {
+      var source = this.context.createBufferSource()
+      source.connect(this.context.destination)
+      source.buffer = this.buffers[name]
+      source.loop = false
+      source.start(0)
+    }
   }
 }
 
