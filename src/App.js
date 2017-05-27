ce2265ce1762dbf87a282e9f4aeb71957386f5bb
Gleb <mail@g1eb.com>
2017-05-27 17:27:52 +0200

    Removed the leading slash from the sample source paths.

--- src/App.js
+++ src/App.js
@@ -14,21 +14,21 @@ class App extends React.Component {
 
     this.state = {
       samples: [
-        {'src': '/audio/11.mp3', 'name': '81', 'color': '#FF875C'},
-        {'src': '/audio/12.mp3', 'name': '87', 'color': '#FF7847'},
-        {'src': '/audio/13.mp3', 'name': '69', 'color': '#FF6933'},
-        {'src': '/audio/14.mp3', 'name': '82', 'color': '#FF5A1F'},
-        {'src': '/audio/15.mp3', 'name': '84', 'color': '#FF4500'},
-        {'src': '/audio/21.mp3', 'name': '65', 'color': '#70D7FF'},
-        {'src': '/audio/22.mp3', 'name': '83', 'color': '#5CD1FF'},
-        {'src': '/audio/23.mp3', 'name': '68', 'color': '#47CBFF'},
-        {'src': '/audio/24.mp3', 'name': '70', 'color': '#33C5FF'},
-        {'src': '/audio/25.mp3', 'name': '71', 'color': '#1FBFFF'},
-        {'src': '/audio/31.mp3', 'name': '90', 'color': '#C247FF'},
-        {'src': '/audio/32.mp3', 'name': '88', 'color': '#BB33FF'},
-        {'src': '/audio/33.mp3', 'name': '67', 'color': '#B41FFF'},
-        {'src': '/audio/34.mp3', 'name': '86', 'color': '#AD0AFF'},
-        {'src': '/audio/35.mp3', 'name': '66', 'color': '#A300F5'},
+        {'src': 'audio/11.mp3', 'name': '81', 'color': '#FF875C'},
+        {'src': 'audio/12.mp3', 'name': '87', 'color': '#FF7847'},
+        {'src': 'audio/13.mp3', 'name': '69', 'color': '#FF6933'},
+        {'src': 'audio/14.mp3', 'name': '82', 'color': '#FF5A1F'},
+        {'src': 'audio/15.mp3', 'name': '84', 'color': '#FF4500'},
+        {'src': 'audio/21.mp3', 'name': '65', 'color': '#70D7FF'},
+        {'src': 'audio/22.mp3', 'name': '83', 'color': '#5CD1FF'},
+        {'src': 'audio/23.mp3', 'name': '68', 'color': '#47CBFF'},
+        {'src': 'audio/24.mp3', 'name': '70', 'color': '#33C5FF'},
+        {'src': 'audio/25.mp3', 'name': '71', 'color': '#1FBFFF'},
+        {'src': 'audio/31.mp3', 'name': '90', 'color': '#C247FF'},
+        {'src': 'audio/32.mp3', 'name': '88', 'color': '#BB33FF'},
+        {'src': 'audio/33.mp3', 'name': '67', 'color': '#B41FFF'},
+        {'src': 'audio/34.mp3', 'name': '86', 'color': '#AD0AFF'},
+        {'src': 'audio/35.mp3', 'name': '66', 'color': '#A300F5'},
       ]
     }
   }
